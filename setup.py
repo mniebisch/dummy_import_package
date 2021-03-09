ad001c2fa5f6d4bc33aeb48d659ef76d9e188109
mniebisch <m.niebisch@gmx.net>
2021-03-09 13:37:39 +0100

    Set up minimal package example.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dummy_import_package',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='#',
+      author='someone',
+      author_email='blub@example.com',
+      license='MIT',
+      packages=['dummy_import_package'],
+      zip_safe=False)
