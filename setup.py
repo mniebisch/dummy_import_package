4b750fb399b842dc6120ee6cf0b627ef9609a5fb
mniebisch <m.niebisch@gmx.net>
2021-03-09 13:41:32 +0100

    Add url to setup.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='dummy_import_package',
       version='0.1',
       description='The funniest joke in the world',
-      url='#',
+      url='https://github.com/mniebisch/dummy_import_package',
       author='someone',
       author_email='blub@example.com',
       license='MIT',
